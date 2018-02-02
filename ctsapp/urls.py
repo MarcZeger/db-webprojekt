4a6d1d0e0c859c34c46932d4eb24da6efe579aea
Marc <marc.zeger98@gmx.de>
2018-02-02 18:39:52 +0100

    vorbereitung für upload der orte

--- ctsapp/urls.py
+++ ctsapp/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('suche/', views.spot_suche, name="suche"),
     path('spot/<int:spot_id>', views.spot_detail, name='spot_detail'),
     path('kontakt/', views.kontakt, name='kontakt'),
-    path('impressum/', views.impressum, name="impressum")
+    path('impressum/', views.impressum, name="impressum"),
+    path('insert/', views.insert, name="insert")
 ]
